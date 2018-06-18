bfbebc196986d233d0b85e8a544e3ea4d2eab87b
Suchitra Naik <susanaik@gmail.com>
2018-06-18 20:04:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,9 +43,9 @@ class ApplicationController < Sinatra::Base
   #   erb :show
   # end
   #
-  # post "/recipes/:id/delete" do
-  #     @recipe = Recipe.find_by_id(params[:id])
-  #     @recipe.delete
-  # end
+  post "/recipes/:id/delete" do
+      @recipe = Recipe.find_by_id(params[:id])
+      @recipe.delete
+  end
 
 end
